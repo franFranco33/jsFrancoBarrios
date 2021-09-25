6b27353856c1c732fd7e7d552cfd86beabc45076
franFranco33 <francoriogallegos93@gmail.com>
2021-09-25 20:08:14 -0300

    puse una palabra en mayúsculas

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-let entrada = prompt("Ingresar una palabra que represente la libertad");
+let entrada = prompt("Ingrese una palabra que para usted represente el concepto de LIBERTAD");
 let salida = entrada + " " + "es libertad";
 alert(salida);
