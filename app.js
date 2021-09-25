05f92801a85279f5ec6a17cd1bbe2eeaf88213cf
franFranco33 <francoriogallegos93@gmail.com>
2021-09-25 19:15:28 -0300

    agregué los 3 archivos (html, css, js)

---
+++ app.js
@@ -0,0 +1,17 @@
+//declarando variables
+let numeroA = 1;
+let numeroB = 2;
+//suma de 2 numeros
+let = resultadoSuma = numeroA + numeroB;
+//resta de 2 numeros
+let = resultadoResta = numeroB - numeroA;
+
+/*let = nombreIngresado = prompt("Ingrese su nombre");
+alert("Hola elTerri, cómo estás?");
+
+console.log("Mensaje de prueba");*/
+
+//ejemplo de script completo
+let entrada = prompt("Ingresar una palabra que represente la libertad");
+let salida = entrada + " " + "es libertad";
+alert(salida);
